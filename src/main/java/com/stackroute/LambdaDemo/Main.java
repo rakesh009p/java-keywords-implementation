20cd6795cbeb251cbcfa84cfea9a14619537d61a
purimetla <rakeshpurimetla@gmail.com>
2019-08-03 15:58:27 +0530

    Lambda

---
+++ src/main/java/com/stackroute/LambdaDemo/Main.java
@@ -0,0 +1,22 @@
+package com.stackroute.LambdaDemo;
+
+public class Main {
+    public static void main(String[] args) {
+        new Thread(new RunnableDemo()).start();
+        System.out.println("normal method");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Anonymous");
+            }
+        });
+
+        new Thread(()->{
+            System.out.println("Lambda");
+            System.out.println("Another Lambda");
+
+        }
+        ).start();
+    }
+
+    }
---
+++ src/main/java/com/stackroute/LambdaDemo/RunnableDemo.java
@@ -0,0 +1,9 @@
+package com.stackroute.LambdaDemo;
+
+public class RunnableDemo implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("running new thread");
+
+    }
+}
